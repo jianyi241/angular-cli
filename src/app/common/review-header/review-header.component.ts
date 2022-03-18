3b2531ff12dcc9b26d0bf8d3b543856e84b9ba6e
song <1281500298@qq.com>
2022-03-18 17:32:25 +0800

    form page

---
+++ src/app/common/review-header/review-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-header',
+  templateUrl: './review-header.component.html',
+  styleUrls: ['./review-header.component.less']
+})
+export class ReviewHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/common/review-layout/review-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-layout',
+  templateUrl: './review-layout.component.html',
+  styleUrls: ['./review-layout.component.less']
+})
+export class ReviewLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
