f505260a80f272381864aca3f7f6087cfd2d8438
Monk <monk@arcadedevhouse.com.au>
2022-03-19 15:43:48 +0800

    config

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: 'https://getway-uat.arcadedevhouse.com/earth-works-test',
+  baseURL: 'http://54.255.39.85/suitability-api-test',
   arcadeAuth: {
     clientId: '38ca222b-1ec7-455b-ab16-6df3d5d10341'
   }
