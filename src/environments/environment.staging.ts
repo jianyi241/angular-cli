db8cce0e4f1f9b1c35ad7a4eb5e1ec3021b7d388
Alex Wang <alex@arcadedevhouse.com.au>
2022-03-24 13:50:11 +0800

    staging配置

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: 'https://getway-uat.arcadedevhouse.com/earth-works-staging',
+  baseURL: 'http://54.206.175.12/suitability-api-staging',
   arcadeAuth: {
     clientId: '88ebd822-8fe2-4b45-9d00-09b18f314b38'
   }
