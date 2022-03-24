9a99d8ab231ea1d091c5a67e7aceffb516fd9152
Monk <monk@arcadedevhouse.com.au>
2022-03-24 17:07:29 +0800

    config change

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  baseURL: 'http://54.255.39.85/suitability-api-test',
+  baseURL: 'http://54.206.175.12/suitability-api-test',
   arcadeAuth: {
     clientId: '38ca222b-1ec7-455b-ab16-6df3d5d10341'
   }
