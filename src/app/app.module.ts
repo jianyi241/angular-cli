792e249fde2f4db0e3073c5e024abfec94349aa5
高志祥 <525825046@qq.com>
2022-04-02 16:31:59 +0800

    feature selection page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,34 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
-import {AppComponent} from './app.component';
-import {AuthModule} from './pages/auth/auth.module';
-
-import {APP_BASE_HREF, CommonModule} from '@angular/common';
-import {OWL_DATE_TIME_LOCALE, OwlDateTimeModule, OwlNativeDateTimeModule} from 'ng-pick-datetime';
-import {ToastRepository} from './repository/toast-repository';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {HttpResultInterceptor} from './interceptor/http-result.interceptor';
-import {LayoutObservable} from './observable/layout-observable';
-import {LocalStorageObServable} from './observable/local-storage-observable';
-import {NgxLoadingSpinnerModule} from '@k-adam/ngx-loading-spinner';
-import {NgbAlertModule, NgbModule, NgbPaginationModule,NgbAccordionModule} from '@ng-bootstrap/ng-bootstrap';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppRoutingModule} from './config/app-routing-module';
-import {NgSelectModule} from '@ng-select/ng-select';
-import {TimePipe} from './pipes/time.pipe';
-import {MessagingService} from './service/messaging.service';
-import {LayoutComponent} from './common/layout/layout.component';
-import {HeaderComponent} from './common/header/header.component';
-import {FooterComponent} from './common/footer/footer.component';
-import {ModalModule} from 'ngx-bootstrap/modal';
-import {NgxFileDropModule} from 'ngx-file-drop';
-import {UserRepository} from './repository/user-repository';
-import {CKEditorModule} from 'ckeditor4-angular';
-import {SystemModule} from "./pages/system/system.module";
-import {ReviewsModule} from "./pages/reviews/reviews.module";
-import {ReviewLayoutComponent} from './common/review-layout/review-layout.component';
-import {ReviewHeaderComponent} from './common/review-header/review-header.component';
-import {ScrollService} from "./service/scroll.service";
+import { APP_BASE_HREF, CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxLoadingSpinnerModule } from '@k-adam/ngx-loading-spinner';
+import { NgbAccordionModule, NgbAlertModule, NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CKEditorModule } from 'ckeditor4-angular';
+import { OwlDateTimeModule, OwlNativeDateTimeModule, OWL_DATE_TIME_LOCALE } from 'ng-pick-datetime';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { HeaderComponent } from './common/header/header.component';
+import { LayoutComponent } from './common/layout/layout.component';
+import { ReviewHeaderComponent } from './common/review-header/review-header.component';
+import { ReviewLayoutComponent } from './common/review-layout/review-layout.component';
+import { AppRoutingModule } from './config/app-routing-module';
+import { HttpResultInterceptor } from './interceptor/http-result.interceptor';
+import { LayoutObservable } from './observable/layout-observable';
+import { LocalStorageObServable } from './observable/local-storage-observable';
+import { AuthModule } from './pages/auth/auth.module';
+import { ReviewsModule } from "./pages/reviews/reviews.module";
+import { SystemModule } from "./pages/system/system.module";
+import { TimePipe } from './pipes/time.pipe';
+import { ToastRepository } from './repository/toast-repository';
+import { UserRepository } from './repository/user-repository';
+import { MessagingService } from './service/messaging.service';
+import { ScrollService } from "./service/scroll.service";
 
 @NgModule({
     declarations: [
@@ -65,10 +63,11 @@ import {ScrollService} from "./service/scroll.service";
         SystemModule,
         ReviewsModule,
         NgbAccordionModule,
+
     ],
     providers: [
 
-        {provide: APP_BASE_HREF, useValue: '/'},
+        { provide: APP_BASE_HREF, useValue: '/' },
         {
             provide: OWL_DATE_TIME_LOCALE, useValue: 'en-au'
         },
