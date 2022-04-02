6222fcb56c24fff18650c435714822d6337f31a5
song <1281500298@qq.com>
2022-04-02 13:35:32 +0800

    new page

--- src/app/directive/table-scroll/table-scroll.directive.ts
+++ src/app/directive/table-scroll/table-scroll.directive.ts
@@ -7,23 +7,30 @@ import $ from 'jquery';
 export class TableScrollDirective {
   @Output() scrollEvent = new EventEmitter<boolean>();
   @Input() maxScrollTop :number;
+  @Input() footHeight :number = 0;
 
   constructor(private elementRef: ElementRef,
               private renderer: Renderer2,) {
   }
 
   @HostListener('scroll',['$event']) onScroll($event) {
+
     let scrollTop = $event.target.scrollTop;
+    let element_Height =this.elementRef.nativeElement.clientHeight;
+    let element_Scroll_Height =this.elementRef.nativeElement.scrollHeight;
     let element = this.elementRef.nativeElement;
-    if(scrollTop > this.maxScrollTop){
-        $(element).addClass('table-review-fixed');
-        this.scrollEvent.emit(true);
+
+    if(element_Scroll_Height < element_Height + this.maxScrollTop){
+      return;
     }
 
+    if(scrollTop > this.maxScrollTop ){
+      $(element).addClass('table-review-fixed');
+      this.scrollEvent.emit(true);
+    }
     else {
       $(element).removeClass('table-review-fixed');
       this.scrollEvent.emit(false);
     }
   }
-
 }
