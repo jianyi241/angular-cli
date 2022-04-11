ad3988c176b4fe0c7edf7c15ec33abe98d003d2c
Monk <monk@arcadedevhouse.com.au>
2022-04-11 09:23:34 +0800

    bug fixed

--- src/app/pages/system/platforms/prop-component/long-text/long-text.component.ts
+++ src/app/pages/system/platforms/prop-component/long-text/long-text.component.ts
@@ -26,7 +26,7 @@ export class LongTextComponent implements OnInit {
     prop_class: string;
 
     constructor() {
-        this.config.editorplaceholder = '';
+        this.config.editorplaceholder = this.prop?.name || '';
     }
 
     ngOnInit(): void {
