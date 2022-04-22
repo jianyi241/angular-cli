2f32b12f543064417fa56540d6ecc40d43a91114
高志祥 <525825046@qq.com>
2022-04-22 10:23:41 +0800

    signup  style change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,18 @@
-export const environment = {
-  production: false,
-  baseURL: 'http://localhost:8080/suitability',
-  arcadeAuth: {
-    clientId: '2112b8b7-37ff-4831-929d-d411895f8a71'
-  }
-};
+// export const environment = {
+//   production: false,
+//   baseURL: 'http://localhost:8080/suitability',
+//   arcadeAuth: {
+//     clientId: '2112b8b7-37ff-4831-929d-d411895f8a71'
+//   }
+// };
 
-/*export const environment = {
+export const environment = {
   production: false,
   baseURL: 'http://54.206.175.12/suitability-api-test',
   arcadeAuth: {
     clientId: '38ca222b-1ec7-455b-ab16-6df3d5d10341'
   }
-};*/
+};
 
 
 // export const environment = {
