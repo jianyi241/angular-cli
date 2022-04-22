12fc5a739bda42c9bd56eadf987892d00ab26fda
高志祥 <525825046@qq.com>
2022-04-22 10:25:09 +0800

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,24 +1,16 @@
-// export const environment = {
-//   production: false,
-//   baseURL: 'http://localhost:8080/suitability',
-//   arcadeAuth: {
-//     clientId: '2112b8b7-37ff-4831-929d-d411895f8a71'
-//   }
-// };
-
 export const environment = {
   production: false,
-  baseURL: 'http://54.206.175.12/suitability-api-test',
+  baseURL: 'http://localhost:8080/suitability',
   arcadeAuth: {
-    clientId: '38ca222b-1ec7-455b-ab16-6df3d5d10341'
+    clientId: '2112b8b7-37ff-4831-929d-d411895f8a71'
   }
 };
 
-
 // export const environment = {
 //   production: false,
-//   baseURL: 'http://54.206.175.12/suitability-api-staging',
+//   baseURL: 'http://54.206.175.12/suitability-api-test',
 //   arcadeAuth: {
-//     clientId: '88ebd822-8fe2-4b45-9d00-09b18f314b38'
+//     clientId: '38ca222b-1ec7-455b-ab16-6df3d5d10341'
 //   }
 // };
+
