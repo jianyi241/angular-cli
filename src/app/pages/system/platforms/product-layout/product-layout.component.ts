f233dbcf67c8024789351c48506efbf9b8476f7f
monk233 <amazingmonk@qq.com>
2022-05-15 15:42:35 +0800

    bug fixed

--- src/app/pages/system/platforms/product-layout/product-layout.component.ts
+++ src/app/pages/system/platforms/product-layout/product-layout.component.ts
@@ -1,21 +1,21 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {ConfigService} from "../../../../service/config.service";
-import {PlatformRepository} from "../../../../repository/platform-repository";
-import {Constants} from "../../../../model/constants";
-import {VersionRepository} from "../../../../repository/version-repository";
-import {Version} from "../../../../model/po/version";
-import {TabType} from "../../../../model/enums/tab-type";
-import {ProductInfo} from "../../../../model/po/productInfo";
-import {ToastRepository} from "../../../../repository/toast-repository";
-import * as moment from "moment";
-import {SaveService} from "../../../../service/save.service";
-import {environment} from "../../../../../environments/environment";
-import {VersionType} from "../../../../model/enums/version-type";
-import {FocusService} from "../../../../service/focus.service";
-import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {RejectModalComponent} from "../modal/reject-modal/reject-modal.component";
-import {CurrentUserService} from "../../../../service/current-user.service";
+import {ActivatedRoute, Router} from '@angular/router';
+import {ConfigService} from '../../../../service/config.service';
+import {PlatformRepository} from '../../../../repository/platform-repository';
+import {Constants} from '../../../../model/constants';
+import {VersionRepository} from '../../../../repository/version-repository';
+import {Version} from '../../../../model/po/version';
+import {TabType} from '../../../../model/enums/tab-type';
+import {ProductInfo} from '../../../../model/po/productInfo';
+import {ToastRepository} from '../../../../repository/toast-repository';
+import * as moment from 'moment';
+import {SaveService} from '../../../../service/save.service';
+import {environment} from '../../../../../environments/environment';
+import {VersionType} from '../../../../model/enums/version-type';
+import {FocusService} from '../../../../service/focus.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {RejectModalComponent} from '../modal/reject-modal/reject-modal.component';
+import {CurrentUserService} from '../../../../service/current-user.service';
 import {NgxLoadingSpinnerService} from '@k-adam/ngx-loading-spinner';
 
 @Component({
@@ -28,8 +28,8 @@ export class ProductLayoutComponent implements OnInit {
     changeVersion: Version;
     changeTabs: Array<{ tabType: number }> = new Array<{ tabType: number }>();
     product: ProductInfo = new ProductInfo();
-    from: string = ''; // fromPage
-    supplierSubmitType: string = '';
+    from = ''; // fromPage
+    supplierSubmitType = '';
     currentTab: string;
 
     constructor(private activatedRoute: ActivatedRoute,
@@ -48,10 +48,10 @@ export class ProductLayoutComponent implements OnInit {
 
     ngOnInit(): void {
         this.activatedRoute.queryParams.subscribe(res => {
-            this.from = res.from
-        })
-        let versionId = this.activatedRoute.firstChild.snapshot.params[Constants.VERSION];
-        this.product.id = this.activatedRoute.firstChild.snapshot.params['productId'];
+            this.from = res.from;
+        });
+        const versionId = this.activatedRoute.firstChild.snapshot.params[Constants.VERSION];
+        this.product.id = this.activatedRoute.firstChild.snapshot.params.productId;
         this.version.id = versionId;
         this.getProduct();
         if (versionId != Constants.VERSION) {
@@ -61,21 +61,21 @@ export class ProductLayoutComponent implements OnInit {
         }
         this.getModelPublishChangeFlag();
         this.getChangeTabs();
-        let url = this.activatedRoute.firstChild?.snapshot?.url;
+        const url = this.activatedRoute.firstChild?.snapshot?.url;
         this.currentTab = url && url.length > 0 ? url[0].path : undefined;
     }
 
     getVersion(): void {
         this.versionRepository.versionById(this.version.id).subscribe(res => {
             this.version = res.data || this.version;
-            this.configService.currentVersion = res.data || this.version
-        })
+            this.configService.currentVersion = res.data || this.version;
+        });
     }
 
     getProduct(): void {
         this.platformRepository.productDetail(this.product.id).subscribe(res => {
             this.product = res.data || this.product;
-        })
+        });
     }
 
     getChangeTabs(): void {
@@ -87,37 +87,37 @@ export class ProductLayoutComponent implements OnInit {
     showEditButton(type: string) {
         if (this.currentUserService.isAdminUser()) {
             if (type === 'reject' || type === 'approve') {
-                return this.version.type === 'Draft' && this.version.versionStatus === this.configService.versionStatus.wait
+                return this.version.type === 'Draft' && this.version.versionStatus === this.configService.versionStatus.wait;
             } else if (type === 'edit') {
-                return this.version.type === 'Publish'
+                return this.version.type === 'Publish';
             }
         } else if (this.currentUserService.isSupplierUser()) {
             if (type === 'edit') {
-                return this.version.type === 'Publish'
+                return this.version.type === 'Publish';
                 // && !this.configService.isEditable(this.version.type)
             } else if (type === 'submit') {
-                return this.version.type === 'Draft' && (this.version.versionStatus === this.configService.versionStatus.normal || this.version.versionStatus === this.configService.versionStatus.rejected) && this.supplierSubmitType === 'submit'
+                return this.version.type === 'Draft' && (this.version.versionStatus === this.configService.versionStatus.normal || this.version.versionStatus === this.configService.versionStatus.rejected) && this.supplierSubmitType === 'submit';
             } else if (type === 'updateStatus') {
-                return this.version.type === 'Draft' && (this.version.versionStatus === this.configService.versionStatus.normal || this.version.versionStatus === this.configService.versionStatus.rejected) && this.supplierSubmitType === 'updateStatus'
+                return this.version.type === 'Draft' && (this.version.versionStatus === this.configService.versionStatus.normal || this.version.versionStatus === this.configService.versionStatus.rejected) && this.supplierSubmitType === 'updateStatus';
             }
         }
     }
 
     ifShowTip(type: string) {
         if (this.version.type === 'History') {
-            return false
+            return false;
         }
         if (this.currentUserService.isSupplierUser()) {
             if (type === 'frozen') {
-                return this.version.versionStatus === this.configService.versionStatus.frozen
+                return this.version.versionStatus === this.configService.versionStatus.frozen;
             } else if (type === 'rejected') {
-                return this.version.versionStatus === this.configService.versionStatus.rejected
+                return this.version.versionStatus === this.configService.versionStatus.rejected;
             }
         }
         if (type === 'oneOrMore') {
-            return this.version.type && this.version.versionStatus !== this.configService.versionStatus.rejected && this.version.versionStatus !== this.configService.versionStatus.frozen && this.changeVersion
+            return this.version.type && this.version.versionStatus !== this.configService.versionStatus.rejected && this.version.versionStatus !== this.configService.versionStatus.frozen && this.changeVersion;
         }
-        return false
+        return false;
     }
 
     getModelPublishChangeFlag(): void {
@@ -127,27 +127,27 @@ export class ProductLayoutComponent implements OnInit {
     }
 
     updateVersionStatus(status: string): void {
-        const _version = JSON.parse(JSON.stringify(this.version))
-        _version.versionStatus = status
+        const _version = JSON.parse(JSON.stringify(this.version));
+        _version.versionStatus = status;
         if (status === this.configService.versionStatus.frozen) {
-            this.loadingService.show()
+            this.loadingService.show();
         }
         this.versionRepository.updateVersionStatus(_version).subscribe(res => {
             if (res.statusCode !== 200) {
-                this.toastRepository.showDanger(res.msg || 'Failed operation')
-                this.loadingService.hide()
-                return
+                this.toastRepository.showDanger(res.msg || 'Failed operation');
+                this.loadingService.hide();
+                return;
             }
-            this.version = res.data
+            this.version = res.data;
             if (this.version.versionStatus === this.configService.versionStatus.rejected) {
-                this.toastRepository.showDanger('Changes have been rejected.')
-                this.loadingService.hide()
+                this.toastRepository.showDanger('Changes have been rejected.');
+                this.loadingService.hide();
             } else {
-                this.toastRepository.showSuccess(res.msg || 'Successful operation')
-                this.loadingService.hide()
+                this.toastRepository.showSuccess(res.msg || 'Successful operation');
+                this.loadingService.hide();
             }
         }, err => {
-        })
+        });
     }
 
     editProduct(): void {
@@ -160,28 +160,28 @@ export class ProductLayoutComponent implements OnInit {
                 return;
             }
             this.version = res.data || this.version;
-            let urlSegment = this.activatedRoute.firstChild.snapshot.url[0];
-            this.getProjectButtonFlag()
-            this.router.navigateByUrl(`/platform/product-tab/${urlSegment.path}/${this.product.id}/${this.version.id}`)
+            const urlSegment = this.activatedRoute.firstChild.snapshot.url[0];
+            this.getProjectButtonFlag();
+            this.router.navigateByUrl(`/platform/product-tab/${urlSegment.path}/${this.product.id}/${this.version.id}`);
         });
     }
 
     getProjectButtonFlag() {
         this.platformRepository.getProductButtonFlag({productId: this.product.id}).subscribe(res => {
-            this.supplierSubmitType = res.msg
+            this.supplierSubmitType = res.msg;
         }, err => {
-        })
+        });
     }
 
     publishProduct(): void {
         if (this.saveService.saveCheck(environment.baseURL + `/product/publish/${this.product.id}`)) {
             return;
         }
-        //设置定时器, 解决失焦问题
-        //当页面控件保留焦点时点击按钮, 会先触发按钮点击事件然后触发失焦保存事件
+        // 设置定时器, 解决失焦问题
+        // 当页面控件保留焦点时点击按钮, 会先触发按钮点击事件然后触发失焦保存事件
         if (this.focusService.hasFocus()) {
             this.focusService.waitBlur(() => {
-                this.publish()
+                this.publish();
             });
         } else {
             this.publish();
@@ -195,13 +195,13 @@ export class ProductLayoutComponent implements OnInit {
                 return;
             }
             this.version = res.data || this.version;
-            let urlSegment = this.activatedRoute.firstChild.snapshot.url[0];
+            const urlSegment = this.activatedRoute.firstChild.snapshot.url[0];
             this.router.navigateByUrl(`/`, {
                 skipLocationChange: true
             }).then(r => {
-                this.router.navigate([`/platform/product-tab/${urlSegment.path}/${this.product.id}/${this.version.id}`])
-            })
-        })
+                this.router.navigate([`/platform/product-tab/${urlSegment.path}/${this.product.id}/${this.version.id}`]);
+            });
+        });
     }
 
     chooseTab(tab: string): void {
@@ -215,19 +215,19 @@ export class ProductLayoutComponent implements OnInit {
     }
 
     getVersionName() {
-        return `Release ${moment(this.version.updateTime).format('D MMM YY')}`
+        return `Release ${moment(this.version.updateTime).format('D MMM YY')}`;
     }
 
     getVersionInfo() {
-        return `Submitted ${moment(this.version.updateTime).format('h:mma D MMM YY')} by Recep Peker`
+        return `Submitted ${moment(this.version.updateTime).format('h:mma D MMM YY')} by Recep Peker`;
     }
 
     backPage() {
-        const versionType = this.version.type
+        const versionType = this.version.type;
         if (versionType !== 'History' && this.from !== 'history') {
-            this.router.navigateByUrl('/platform/product')
+            this.router.navigateByUrl('/platform/product');
         } else {
-            this.backHistory()
+            this.backHistory();
         }
     }
 
@@ -236,7 +236,7 @@ export class ProductLayoutComponent implements OnInit {
             this.version = res.data || this.version;
             this.version.id = res.data?.id || Constants.VERSION;
             this.chooseTab(TabType.changeHistory.name);
-        })
+        });
     }
 
     rejectUpdate(): void {
@@ -246,10 +246,10 @@ export class ProductLayoutComponent implements OnInit {
             centered: true
         });
         modalRef.result.then(res => {
-            console.log('confirm')
-            this.updateVersionStatus('Rejected')
+            console.log('confirm');
+            this.updateVersionStatus('Rejected');
         }, err => {
-            console.log('cancel')
-        })
+            console.log('cancel');
+        });
     }
 }
