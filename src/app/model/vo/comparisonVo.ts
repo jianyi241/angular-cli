a51124063c68c7b971ff3397a4a909bbf468090a
Monk <monk@arcadedevhouse.com.au>
2022-05-24 15:17:45 +0800

    comparison & due

--- src/app/model/vo/comparisonVo.ts
+++ src/app/model/vo/comparisonVo.ts
@@ -2,6 +2,7 @@ import {ComparisonInfo} from "../po/comparisonInfo";
 
 
 export class ComparisonVo extends ComparisonInfo{
+    clientName?: string;
     mainPlatformCheck = true;
     feeProducts: Array<string> = new Array<string>();
     nonFeeProducts: Array<string> = new Array<string>();
