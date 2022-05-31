ffa6b2e0ccb6783f2bc4b6887d95c4118ae80949
Monk <monk@arcadedevhouse.com.au>
2022-05-31 17:44:02 +0800

    bug fixed

--- src/app/directive/show-more.directive.ts
+++ src/app/directive/show-more.directive.ts
@@ -2,26 +2,44 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 import $ from 'jquery';
 
 @Directive({
-  selector: '[appShowMore]'
+    selector: '[appShowMore]'
 })
 export class ShowMoreDirective {
     maxHeight = 170;
     maxWidth = 198;
 
-  constructor(private renderer: Renderer2,private elementRef: ElementRef) {
+    constructor(private renderer: Renderer2, private elementRef: ElementRef) {
 
-      let element = this.elementRef.nativeElement;
+        let element = this.elementRef.nativeElement;
 
-      setTimeout(()=> {
-          let $1 = $(this.elementRef.nativeElement);
-          let text = $1.text();
-          let _clientHeight = this.elementRef.nativeElement.clientHeight;
-          let _clientWidth = this.elementRef.nativeElement.clientWidth;
-          if(text.length > 255 && (_clientHeight > this.maxHeight || _clientWidth > this.maxWidth)){
-                this.renderer.addClass(this.elementRef.nativeElement,'detail-content-more');
-                $(element).siblings('.more-link').show();
-          }
-      },10);
+        setTimeout(() => {
+            // @ts-ignore
+            new Cuttr(element, {
+                //options here
+                truncate: 'characters',
+                length: 550,
+                readMoreBtnPosition: 'inside',
+                ending: '<span class="more-link" style="color: #4640de;font-weight: 700;font-family: \'Epilogue\'; font-size: 12px; line-height: 17px; letter-spacing: -0.04em;cursor: pointer;height: 18px;">...More</span>'
+            });
+            $(element).find('.more-link').click(e => {
+                e.stopPropagation();
+                $(element).siblings('.popover-custom').click(e => {
+                    e.stopPropagation();
+                })
+                let pop = $(element).siblings('.popover-custom');
+                //使Pop展示位置居中, 偏移量 + monk-link高度50% - 箭头高度50% - pop高度50%
+                let number = e.target.offsetTop + 9 - 5 - pop.height() / 2;
+                pop.css('transform', `translate(${e.target.offsetLeft + 40}px, ${number}px)`)
+                pop.show();
+                pop.find('.icon-close').click(function ($event) {
+                    $(this).parents('.popover-custom').hide();
+                })
+                $(document).one('click', (e) => {
+                    e.stopPropagation();
+                    $(element).siblings('.popover-custom').hide();
+                })
+            })
+        }, 10);
 
-  }
+    }
 }
